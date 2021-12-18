d543c67f55603547d08a42bc9057aa45ac68f209
jocyber <jordaneharman@protonmail.com>
2021-12-18 18:48:53 -0500

    added database file and db model in app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
 
 #return the index.html file
 @app.route('/')
