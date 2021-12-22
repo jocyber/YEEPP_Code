e4a212b0f26c358f96df0e26670497b4ef820cf6
jocyber <jordaneharman@protonmail.com>
2021-12-22 17:11:26 -0500

    added some stuff to app.py

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
+#not sure what the hell I'm doing here, yet
+class Problems(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    acceptance = db.Column(db.Float)
+    difficulty = db.Column(db.String(10))
+
+    def __init__(self, title, difficulty):
+        self.title = title
+        self.difficulty = difficulty
+
 #return the index.html file
 @app.route('/')
 def index():
