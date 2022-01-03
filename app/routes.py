7966a1d0b43518532ef9cf2a26dfdaeaa5add6c5
jocyber <jordaneharman@protonmail.com>
2022-01-03 16:46:55 -0500

    added a textarea for the code editor

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,10 @@ def index():
     #checks templates directory by default
     return render_template("index.html")
 
+@app.route('/test')
+def testCode():
+    return render_template("test.html")
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
