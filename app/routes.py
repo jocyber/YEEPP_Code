b371d5d7244d725b5614722c9ff4fd665d7b16f1
jocyber <jordaneharman@protonmail.com>
2022-01-05 11:35:18 -0500

    uodated the 'test' function in routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request, redirect
 from app import app
 from app.models import Problems
+import os
 
 #return the index.html file
 @app.route('/')
@@ -8,9 +9,15 @@ def index():
     #checks templates directory by default
     return render_template("index.html")
 
-@app.route('/test')
-def testCode():
-    return render_template("test.html")
+#function for handling the users code
+@app.route('/test', methods=["GET", "POST"])
+def testPage():
+    if request.method == "POST":
+        code = request.form("codeEdit") #name of input from html file
+        #run the code through the sandboxed environment
+        return redirect(url_for("test")) #return to the test page
+    else:
+        return render_template("test.html")
 
 @app.errorhandler(404)
 def not_found(e):
